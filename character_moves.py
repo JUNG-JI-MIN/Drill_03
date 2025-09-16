5355d814297546c2948818e05cb5b9c85006292a
JUNG JI MIN <wlals04977@naver.com>
2025-09-16 20:57:22 +0900

    캐릭터 출력 구현

--- character_moves.py
+++ character_moves.py
@@ -2,6 +2,7 @@ from pico2d import *
 
 open_canvas()
 
+character = load_image('character.png')
 def move_rectangle():
     print("Hello World, This is rectangle")
     pass
@@ -14,6 +15,8 @@ def move_circle():
 
 # fill here
 while True:
+    clear_canvas_now()
+    character.draw_now(400, 90)
     move_circle()
     move_rectangle()
     pass
