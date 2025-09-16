162dfc4e396e7e5bbf001823ae67ccfadf1ec0aa
JUNG JI MIN <wlals04977@naver.com>
2025-09-16 21:14:25 +0900

    사각 운동 형태 완성

--- character_moves.py
+++ character_moves.py
@@ -3,8 +3,24 @@ from pico2d import *
 open_canvas()
 
 character = load_image('character.png')
+grass = load_image('grass.png')
+
+def move_right():
+    pass
+def move_left():
+    pass
+def move_up():
+    pass
+def move_down():
+    pass
+
 def move_rectangle():
     print("Hello World, This is rectangle")
+    move_right() #화면 절반 이동
+    move_up() #화면 전부 이동
+    move_left() #화면 전부 이동
+    move_down() #화면 전부 이동
+    move_right() #화면 절반 이동
     pass
 
 
@@ -14,14 +30,16 @@ def move_circle():
         angle = math.radians(i)
         x = 400 + radius * math.cos(angle)
         y = 300 + radius * math.sin(angle)
-        clear_canvas_now()
+        clear_canvas()
         character.draw_now(x, y)
+        grass.draw_now(400, 30)
         delay(0.01)
     pass
 
 # fill here
 while True:
     move_circle()
+
     move_rectangle()
     break
     #pass
