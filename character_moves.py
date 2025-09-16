7939b36fb22600934deac02d3f6511340d51640c
JUNG JI MIN <wlals04977@naver.com>
2025-09-16 16:30:16 +0900

    사각 운동, 원 운동 뼈대 생성

--- character_moves.py
+++ character_moves.py
@@ -2,8 +2,18 @@ from pico2d import *
 
 open_canvas()
 
+def move_rectangle():
+    pass
+
+
+def move_circle():
+    pass
+
+
 # fill here
 while True:
+    move_circle()
+    move_rectangle()
     pass
 
 
